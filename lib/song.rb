54e0e01a0162d33dcc46e09cf110dca67230855e
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-04 01:48:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,15 +3,16 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
-  @@genres = {}
+  @@genres = []
 
 
   def initialize(song_name, song_artist, song_genre)
-    @@count += 1
+    @@count += 1    
     @name = song_name
     @artist = song_artist
-    @@artists << song_artist
     @genre = song_genre
+    @@artists << song_artist
+    @@genres << song_genre
   end
 
   def self.song_count
